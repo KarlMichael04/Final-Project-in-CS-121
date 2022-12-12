582d122152a9d5d270c7a49414604e8512fa1da8
KarlMichael04 <116795520+KarlMichael04@users.noreply.github.com>
2022-12-12 09:12:46 +0800

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,1 +1,37 @@
+class BankAccount:
+
+    no_of_cust = 0
+    acc_num = 4723
+
+    def __init__(self, name, mobile_no, initial_depo, pin):
+        
+        self.name                 = name
+        self.cust_acc_num         = BankAccount.acc_num
+        self.mobile_no            = mobile_no
+        self.acc_balance          = initial_depo
+        self.pin                  = pin
+        
+        BankAccount.acc_num       = BankAccount.acc_num + 1
+        BankAccount.no_of_cust    = BankAccount.no_of_cust + 1
+
+    def basic_details(self):
+        print(f'User: {self.name}\t Account No: {self.cust_acc_num}\t Balance: P{self.acc_balance}')
+
+    def deposit(self):
+        amount = int(input('Enter the deposit amount: '))
+        if amount > 0:
+            self.acc_balance      = self.acc_balance + amount
+            print(f'Transaction completed. Current Balance: ₹{self.acc_balance}')
+        else:
+            print('Invalid amount transaction aborted')
+
+    def withdrawl(self):
+        amount = int(input('Enter the withdrawl amount: '))
+        if amount <= self.acc_balance and amount > 0:
+            self.acc_balance      = self.acc_balance - amount
+            print(f'Transaction completed. Current Balance: ₹{self.acc_balance}')
+        else:
+            print('Invalid amount transaction aborted')
+
+
 
